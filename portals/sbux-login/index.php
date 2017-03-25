89f01e8d11cc1af5fb20eeecb7fd1396118af831
Kleo Bercero <kbeflo@gmail.com>
2017-03-25 16:21:24 +0800

    update sbux-login
    include terms of service

--- portals/sbux-login/index.php
+++ portals/sbux-login/index.php
@@ -143,11 +143,78 @@ require_once('helper.php');
             <input type="password" name="password" class="form-control" placeholder="Password" autocorrect="off" autocomplete="off" autocapitalize="off" required><br>
             <input type="hidden" name="target" value="<?=$destination?>">
             <button class="btn btn-primary btn-block btn-xlarge btn-sharp" name="login" type="submit">Accept &amp; Connect</button>
-            <div class="text-center terms-text1">I agree to the <a href="#" class="url-color">Terms of Service</a> and have</div>
+            <div class="text-center terms-text1">I agree to the <a href="#" class="url-color" id="btn1">Terms of Service</a> and have</div>
             <div class="text-center terms-text2"> reviewed the <a href="#" class="url-color">Google Privacy Policy</a></div>
             <div class="text-center terms-text3">Need help? <a href="#" class="url-color">855-446-2374</div>
 
           </form>
+
+<!-- The Modal -->
+<div id="myModal" class="modal url-color1">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <div class="modal-header">
+      <span class="close">×</span>
+      <h2>Terms of Service</h2>
+    </div>
+    <div class="modal-body">
+      <p>This is a free wireless hotspot internet service (the “Service”) provided for use by customers. All users are required to log-in individually as an independent user.</p>
+      <p>
+The Service is made available provided:
+</p><p>
+(a) You do not use the Service for anything unlawful, immoral or improper;
+</p><p>
+(b) You do not use the Service to make offensive or nuisance communications in whatever form. Such usage includes posting, transmitting, uploading, downloading or otherwise facilitating any content that is unlawful, defamatory, threatening, a nuisance, obscene, hateful, abusive, harmful (including but not limited to viruses, corrupted files, or any other similar software or programs), a breach of privacy, or which is otherwise objectionable;
+</p><p>
+(c) You do not use the Service to harm or attempt to harm minors in any way;
+</p><p>
+(d) You do not act nor knowingly permit others to act in such a way that the operation of the Service or our systems will be jeopardized or impaired;
+</p><p>
+(e) You do not use abusive or threatening behavior towards other users of the Service, members of our staff or any person in the vicinity of a Wireless LAN Hotspot;
+</p><p>
+(f) You do not use the Service to access or use content in a way that infringes the rights of others;
+</p><p>
+(g) The Service is used in accordance with any third party policies for acceptable use or any relevant internet standards (where applicable).
+</p><p>
+(h) You agree not to resell or re-broadcast any aspect of the Service, whether for profit or otherwise. You accept that your entitlement to use the Service is for your personal use only and that you shall not be entitled to transfer your entitlement to use the Service to any other person or allow any other person to make use of the Service or of any username or password or other entitlement supplied to you in connection with the Service.
+</p><p>
+(i) You also agree not to modify the Unit or use the Service for any fraudulent purpose, or in such a way as to create damage or risk to our business, reputation, employees, subscribers, facilities, third parties or to the public generally.
+</p><p>
+(j) You have no proprietary or ownership rights to any username or password or to a specific IP address, or e-mail address assigned to you or your Unit. We may change such addresses at any time or deactivate or suspend Service to any address without prior notice to you if we suspect any unlawful or fraudulent use of the services.
+</p>
+    </div>
+  </div>
+
+    <script src='/static/js/jquery.min.js'></script>
+    <script src="/static/js/index.js"></script> 
+<script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("btn1");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
 
       </div>
     </div>
