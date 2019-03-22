5adc55a1ba1eb70e7f0837cb0302fadd4283977a
Kleo Bercero <kbeflo@gmail.com>
2019-03-22 13:41:42 +0800

    https/http fix
    https://github.com/frozenjava/EvilPortalNano/pull/26

--- portals/starbucks-login/index.php
+++ portals/starbucks-login/index.php
@@ -1,5 +1,5 @@
 <?php
-$destination = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['HTTP_URI'] . "";
+$destination = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 require_once('helper.php');
 ?>
 
