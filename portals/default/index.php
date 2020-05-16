4a397963c4330f53613750a62da77055bceebf10
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2020-05-16 17:10:33 +0200

    Update index.php
    Added The mysterious Landing Page Browser Stats Script :-)

--- portals/default/index.php
+++ portals/default/index.php
@@ -1,6 +1,45 @@
 <?php
 $destination = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 require_once('helper.php');
+function increment_browser($browser)
+{
+    try {
+        $sqlite = new \SQLite3('/tmp/landingpage.db');
+    } catch (Exception $e) {
+        return false;
+    }
+    $sqlite->exec('CREATE TABLE IF NOT EXISTS user_agents  (browser TEXT NOT NULL);');
+    $statement = $sqlite->prepare('INSERT INTO user_agents (browser) VALUES(:browser);');
+    $statement->bindValue(':browser', $browser, SQLITE3_TEXT);
+    try {
+        $ret = $statement->execute();
+    } catch (Exception $e) {
+        return false;
+    }
+    return $ret;
+}
+
+function identifyUserAgent($userAgent)
+{
+    if (preg_match('/^Mozilla/', $userAgent)) {
+        if (preg_match('/Chrome\/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/', $userAgent)) {
+            increment_browser('chrome');
+        } elseif (preg_match('/Safari\/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/', $userAgent)) {
+            increment_browser('safari');
+        } elseif (strpos($userAgent, 'MSIE ') || preg_match('/Trident\/[0-9]/', $userAgent)) {
+            increment_browser('internet_explorer');
+        } elseif (preg_match('/Firefox\/[0-9]{1,3}\.[0-9]{1,3}/', $userAgent)) {
+            increment_browser('firefox');
+        } else {
+            increment_browser('other');
+        }
+    } elseif (preg_match('/^Opera\/[0-9]{1,3}\.[0-9]/', $userAgent)) {
+        increment_browser('opera');
+    } else {
+        increment_browser('other');
+    }
+}
+identifyUserAgent($_SERVER['HTTP_USER_AGENT']);
 ?>
 
 <HTML>
